039063fdd8c6f4c26b58fcd2c46bf5bda371952e
cpasjuste <cpasjuste@gmail.com>
2026-07-05 15:44:12 +0200

    - web: fix apache not listening on requested web port - multicast: fix multicast (use host networking) - multicast: fix/set starting address for udp-sender

--- resources/entry.sh
+++ resources/entry.sh
@@ -15,6 +15,18 @@ if [ -z "$(ls -A /images)" ]; then
     tar -zxf /root/images.tar.gz -C /
 fi
 
+echo "fixing web port"
+sed -i "s/:80/:${HTTP_PORT}/g" /etc/apache2/sites-enabled/001-fog.conf
+sed -i "s/Listen 80/Listen ${HTTP_PORT}/g" /etc/apache2/ports.conf
+
+echo "fixing tftpboot"
+sed -i "s/0.0.0.0/${HTTP_ADDRESS}:${HTTP_PORT}/g" /tftpboot/default.ipxe
+
+echo "fixing multicast"
+# add host (HTTP_ADDRESS) address to "know ips"
+# https://github.com/FOGProject/fogproject/blob/stable/packages/web/lib/fog/fogbase.class.php
+sed -i "s/127.0.1.1')/127.0.1.1'), array('${HTTP_ADDRESS}', '${HTTP_ADDRESS}')/g" /var/www/fog/lib/fog/fogbase.class.php
+
 echo "starting required services"
 /etc/init.d/mariadb start
 /etc/init.d/php8.4-fpm start
@@ -28,14 +40,6 @@ echo "/images *(ro,sync,no_wdelay,subtree_check,insecure_locks,all_squash,anonui
 echo "/images/dev *(rw,async,no_wdelay,subtree_check,all_squash,anonuid=1000,anongid=1000,fsid=1)" >> /etc/exports
 /etc/init.d/nfs-kernel-server start
 
-echo "fixing tftpboot"
-sed -i "s/0.0.0.0/${HTTP_ADDRESS}:${HTTP_PORT}/g" /tftpboot/default.ipxe
-
-echo "fixing multicast"
-# add host (HTTP_ADDRESS) ip address to "know ips"
-# https://github.com/FOGProject/fogproject/blob/stable/packages/web/lib/fog/fogbase.class.php
-sed -i "s/127.0.1.1')/127.0.1.1'), array('${HTTP_ADDRESS}', '${HTTP_ADDRESS}')/g" /var/www/fog/lib/fog/fogbase.class.php
-
 echo "fixing databse and passwords"
 printf '%s:%s\n' 'fogproject' "${STORAGE_PASSWORD}" | chpasswd
 perl -0pi -e 's/\Q#docker-fog-password#\E/$ENV{STORAGE_PASSWORD}/g;' /var/www/fog/lib/fog/config.class.php
@@ -44,8 +48,10 @@ mysql -hlocalhost -uroot -e "ALTER USER 'fog'@'localhost' IDENTIFIED BY '${MYSQL
 mysql -hlocalhost -uroot -D fog -e "UPDATE users SET uPass = MD5('NEW_PASSWORD') WHERE uName = 'fog';"
 mysql -hlocalhost -uroot -D fog -e "
 UPDATE globalSettings SET settingValue = '${HTTP_ADDRESS}' WHERE settingKey = 'FOG_TFTP_HOST';
-UPDATE globalSettings SET settingValue = '${HTTP_ADDRESS}' WHERE settingKey = 'FOG_WEB_HOST';
+UPDATE globalSettings SET settingValue = '${HTTP_ADDRESS}:${HTTP_PORT}' WHERE settingKey = 'FOG_WEB_HOST';
 UPDATE globalSettings SET settingValue = '${STORAGE_PASSWORD}' WHERE settingKey = 'FOG_TFTP_FTP_PASSWORD';
+UPDATE globalSettings SET settingValue = '55000' WHERE settingKey = 'FOG_UDPCAST_STARTINGPORT';
+UPDATE globalSettings SET settingValue = '55000' WHERE settingKey = 'FOG_MULTICAST_PORT_OVERRIDE';
 UPDATE nfsGroupMembers SET ngmHostname = '${HTTP_ADDRESS}' WHERE ngmID = 1;
 UPDATE nfsGroupMembers SET ngmPass = '${STORAGE_PASSWORD}' WHERE ngmID = 1;
 UPDATE users SET uPass = MD5('${WEB_PASSWORD}') WHERE uName = 'fog';
